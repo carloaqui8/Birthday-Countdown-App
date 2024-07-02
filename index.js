ca195ae533075ff6dd83bc840d486dcf31886a1f
Carlo Aqui <carloaqui.8@gmail.com>
2024-07-01 17:47:21 -0700

    wah

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const myForm = document.getElementsByClassName("myForm");
+// const myForm = document.getElementsByClassName("myForm");
+
+const collapsible = document.getElementsByClassName("collapsible");
+
+collapsible[0].addEventListener("click", () => {
+    collapsible[0].classList.toggle("active");
+    myForm = collapsible[0].nextElementSibling;
+    if (myForm.style.display === "block") {
+        myForm.style.display = "none";
+    }
+    else {
+        myForm.style.display = "block";
+    }
+});
 
 function createCard(name, date) {
     console.log("wah");
